c1884b4082963d42a9e6bfa18c982a3d61b5ea12
masturdating1991 <aligodosi@gmail.com>
2020-08-02 12:10:40 +0430

    first desktop page done

--- src/Components/Opinion-Poll/OpinionPoll.jsx
+++ src/Components/Opinion-Poll/OpinionPoll.jsx
@@ -5,11 +5,19 @@ import './OpinionPoll.scss'
 function OpinionPoll() {
     return (
         <div className='OpinionPoll'>
-            <img className='bgc' src={Data.background_url} />
-            <div class="backdrop">
+            <img className='bgc' src={Data.background_url} alt='Background'/>
+            <div className="backdrop">
                 <h1>فرم نظرسنجی و ارتقا سطح خدمت رسانی</h1>
-                <img src={Data.logo_url} />
+                <img src={Data.logo_url} alt='Logo'/>
                 <button>شروع</button>
+
+                <div className='footer'>
+                    <div className='footer-intro'>
+                        <p>قدرت گرفته از</p>
+                        <a href='#'>Soally</a>
+                    </div>
+                    <p>پلتفرم طراخی فرم های CRM آنلاین</p>
+                </div>
             </div>
         </div>
     )
